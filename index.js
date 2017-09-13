9ee5c22382c396e485c9a2c69000d7be5b85121d
Jeni Williams <stanfordjeni@gmail.com>
2017-09-13 15:56:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,3 +25,18 @@ function appendKitten(name) {
   var newarray = [...kittens, name]
   return newarray
 }
+
+function prependKitten(name) {
+  var newarray = [name, ...kittens]
+  return newarray
+}
+
+function removeLastKitten() {
+  var newarray = kittens.slice(0,len(kittens)-1)
+  return newarray
+}
+
+function removeFirstKitten() {
+  var newarray = kittens.slice(1)
+  return newarray
+}
