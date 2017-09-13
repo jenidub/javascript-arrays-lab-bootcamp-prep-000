47b490828c57b42abccdc4bbce133a4c265c97a8
Jeni Williams <stanfordjeni@gmail.com>
2017-09-13 15:45:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const app = "I don't do much."
 kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name) {
-  kittens.push(name)
-  return kittens
+  newarray = [name, ...kittens]
+  return newarray
 }
